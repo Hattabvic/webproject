3a4590e36c7b4e473c1181fb3fcf3afd689b4cb0
ahmed1882002 <elfakyahmed52@gmail.com>
2024-06-29 20:51:11 +0300

    Edit Some Coding And Add some Function delete page tobolgy

--- front_end/subscriber/home.php
+++ front_end/subscriber/home.php
@@ -128,10 +128,6 @@ $assets = "../assets";
         .mini-profile-widget .details {
             text-align: center;
         }
-
-
-
- 
     </style>
 
 
@@ -200,74 +196,75 @@ $assets = "../assets";
 
 
     <main>
-    <section class="py-5 text-center container">
-    <div class="row py-lg-5">
-      <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">We love you  <?= $user->first_name ?></h1>
-        <h2 class="fw-light">Enjoy our blog</h2>
-       
-      </div>
-    </div>
-  </section>
+        <section class="py-5 text-center container">
+            <div class="row py-lg-5">
+                <div class="col-lg-6 col-md-8 mx-auto">
+                    <h1 class="fw-light">We love you <?= $user->first_name ?></h1>
+                    <h2 class="fw-light">Enjoy our blog</h2>
+
+                </div>
+            </div>
+        </section>
 
         <div class="container ">
             <div class="">
                 <?php foreach ($homePosts as $posts) {  ?>
                     <div class="col-8 offset-2 border border-secondary-subtle rounded mb-4 shadow p-2">
-                         <div class="panel panel-white  rounded">
-                                        <div class="post-heading  m-4">
-                                            <div class="  ">
-                                                <img style="width:50px ; height: 50px; border-radius:50px;"
-                                                    src="<?php if (!empty($posts["user_image"]))
-                                                        echo $posts["user_image"];
-                                                    else
-                                                        echo 'https://t4.ftcdn.net/jpg/02/34/57/59/240_F_234575931_hDnNJiXNgTzJO4iDDZjhneWKF25o7O2f.jpg' ?>">
-                                                    <h6 class="mb-0"><?= $posts["first_name"] ?><?= " " .$posts["last_name"] ?></h6>
-                                                <br>
-                                                <h6 class=""> <span><?= $posts["created_at"] ?></span></h6>
-                                            </div>
+                        <div class="panel panel-white  rounded">
+                            <div class="post-heading  m-4">
+                                <div class="  ">
+                                    <img style="width:50px ; height: 50px; border-radius:50px;" src="<?php if (!empty($posts["user_image"]))
+                                                                                                            echo $posts["user_image"];
+                                                                                                        else
+                                                                                                            echo 'https://t4.ftcdn.net/jpg/02/34/57/59/240_F_234575931_hDnNJiXNgTzJO4iDDZjhneWKF25o7O2f.jpg' ?>">
+                                    <h6 class="mb-0"><?= $posts["first_name"] ?><?= " " . $posts["last_name"] ?></h6>
+                                    <br>
+                                    <h6 class=""> <span><?= $posts["created_at"] ?></span></h6>
+                                </div>
 
-                                        </div>
-                                        <div class="post-image mx-4 my-1">
-                                            <?php if (!empty($posts['image'])) { ?>
-                                                <img class="card-img-top rounded" src="<?= $posts['image'] ?>" alt="title"
-                                                    style="max-height: 900px; object-fit: contain; width: 100%;" />
-                                            <?php } ?>
-                                        </div>
-                                        <div class="card-body px-2">
-                                            <h4 class="card-title ps-3 mx-4"><?= $posts['title'] ?></h4>
-                                            <p class="card-text ps-4 mx-4"><?= $posts['content'] ?></p>
-                                            <p class="card-text mx-5 my-2">
-                                                <small class="text-muted">Published on
-                                                    <?= date('F j, Y, g:i a', strtotime($posts['created_at'])) ?></small>
-                                            </p>
-                                        </div>
-                                        <hr>
+                            </div>
+                            <div class="post-image mx-4 my-1">
+                                <?php if (!empty($posts['image'])) { ?>
+                                    <img class="card-img-top rounded" src="<?= $posts['image'] ?>" alt="title" style="max-height: 900px; object-fit: contain; width: 100%;" />
+                                <?php } ?>
+                            </div>
+                            <div class="card-body px-2">
+                                <h4 class="card-title ps-3 mx-4"><?= $posts['title'] ?></h4>
+                                <p class="card-text ps-4 mx-4"><?= $posts['content'] ?></p>
+                                <p class="card-text mx-5 my-2">
+                                    <small class="text-muted">Published on
+                                        <?= date('F j, Y, g:i a', strtotime($posts['created_at'])) ?></small>
+                                </p>
+                            </div>
+                            <hr>
                             <div class="post-description p-2 d-flex justify-content-between">
 
                                 <div class="stats ">
 
-                                <?php if (!empty($user->myLike($posts["id"], $user->id))) {  ?>
+                                    <?php if (!empty($user->myLike($posts["id"], $user->id))) {  ?>
                                         <a style="color:red" role="button" href="handle_like_home.php?post_id=<?= $posts["id"] ?>&like=no" class="btn ">
                                             <i class="bi bi-heart-fill"></i>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z"/></svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 512 512">
+                                                <path d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z" />
+                                            </svg>
                                             <span>Like</span>
                                         </a>
-                                    <?php } else
-                                    {  ?>
+                                    <?php } else {  ?>
                                         <a style="color:" role="button" href="handle_like_home.php?post_id=<?= $posts["id"] ?>&like=yes" class="btn ">
                                             <i class="bi bi-heart"></i>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M323.8 34.8c-38.2-10.9-78.1 11.2-89 49.4l-5.7 20c-3.7 13-10.4 25-19.5 35l-51.3 56.4c-8.9 9.8-8.2 25 1.6 33.9s25 8.2 33.9-1.6l51.3-56.4c14.1-15.5 24.4-34 30.1-54.1l5.7-20c3.6-12.7 16.9-20.1 29.7-16.5s20.1 16.9 16.5 29.7l-5.7 20c-5.7 19.9-14.7 38.7-26.6 55.5c-5.2 7.3-5.8 16.9-1.7 24.9s12.3 13 21.3 13L448 224c8.8 0 16 7.2 16 16c0 6.8-4.3 12.7-10.4 15c-7.4 2.8-13 9-14.9 16.7s.1 15.8 5.3 21.7c2.5 2.8 4 6.5 4 10.6c0 7.8-5.6 14.3-13 15.7c-8.2 1.6-15.1 7.3-18 15.2s-1.6 16.7 3.6 23.3c2.1 2.7 3.4 6.1 3.4 9.9c0 6.7-4.2 12.6-10.2 14.9c-11.5 4.5-17.7 16.9-14.4 28.8c.4 1.3 .6 2.8 .6 4.3c0 8.8-7.2 16-16 16H286.5c-12.6 0-25-3.7-35.5-10.7l-61.7-41.1c-11-7.4-25.9-4.4-33.3 6.7s-4.4 25.9 6.7 33.3l61.7 41.1c18.4 12.3 40 18.8 62.1 18.8H384c34.7 0 62.9-27.6 64-62c14.6-11.7 24-29.7 24-50c0-4.5-.5-8.8-1.3-13c15.4-11.7 25.3-30.2 25.3-51c0-6.5-1-12.8-2.8-18.7C504.8 273.7 512 257.7 512 240c0-35.3-28.6-64-64-64l-92.3 0c4.7-10.4 8.7-21.2 11.8-32.2l5.7-20c10.9-38.2-11.2-78.1-49.4-89zM32 192c-17.7 0-32 14.3-32 32V448c0 17.7 14.3 32 32 32H96c17.7 0 32-14.3 32-32V224c0-17.7-14.3-32-32-32H32z"/></svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 512 512">
+                                                <path d="M323.8 34.8c-38.2-10.9-78.1 11.2-89 49.4l-5.7 20c-3.7 13-10.4 25-19.5 35l-51.3 56.4c-8.9 9.8-8.2 25 1.6 33.9s25 8.2 33.9-1.6l51.3-56.4c14.1-15.5 24.4-34 30.1-54.1l5.7-20c3.6-12.7 16.9-20.1 29.7-16.5s20.1 16.9 16.5 29.7l-5.7 20c-5.7 19.9-14.7 38.7-26.6 55.5c-5.2 7.3-5.8 16.9-1.7 24.9s12.3 13 21.3 13L448 224c8.8 0 16 7.2 16 16c0 6.8-4.3 12.7-10.4 15c-7.4 2.8-13 9-14.9 16.7s.1 15.8 5.3 21.7c2.5 2.8 4 6.5 4 10.6c0 7.8-5.6 14.3-13 15.7c-8.2 1.6-15.1 7.3-18 15.2s-1.6 16.7 3.6 23.3c2.1 2.7 3.4 6.1 3.4 9.9c0 6.7-4.2 12.6-10.2 14.9c-11.5 4.5-17.7 16.9-14.4 28.8c.4 1.3 .6 2.8 .6 4.3c0 8.8-7.2 16-16 16H286.5c-12.6 0-25-3.7-35.5-10.7l-61.7-41.1c-11-7.4-25.9-4.4-33.3 6.7s-4.4 25.9 6.7 33.3l61.7 41.1c18.4 12.3 40 18.8 62.1 18.8H384c34.7 0 62.9-27.6 64-62c14.6-11.7 24-29.7 24-50c0-4.5-.5-8.8-1.3-13c15.4-11.7 25.3-30.2 25.3-51c0-6.5-1-12.8-2.8-18.7C504.8 273.7 512 257.7 512 240c0-35.3-28.6-64-64-64l-92.3 0c4.7-10.4 8.7-21.2 11.8-32.2l5.7-20c10.9-38.2-11.2-78.1-49.4-89zM32 192c-17.7 0-32 14.3-32 32V448c0 17.7 14.3 32 32 32H96c17.7 0 32-14.3 32-32V224c0-17.7-14.3-32-32-32H32z" />
+                                            </svg>
 
                                             <span>Like</span>
                                         </a>
-                                    <?php } ?>                   
-                              
+                                    <?php } ?>
+
                                 </div>
 
                                 <div class="action">
                                     <a role="button" href="All_like.php?post_id=<?= $posts["id"] ?>" class="btn">
-                                        
+
                                         <i class="fa-solid fa-users-between-lines"></i>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="fa-solid fa-users-between-lines" viewBox="0 0 640 512">
 
@@ -309,10 +306,10 @@ $assets = "../assets";
                                                             <p class="small text-muted mb-0"><?= $comment["created_at"] ?></p>
                                                         </div>
                                                     </div>
-                                                   
-                                                   <div class="ms-3 mt-3 ps-3 pt-3 bg-white border border-secondary-subtle rounded">
-                                                   <p><?= $comment["comment"] ?></p>
-                                                   </div>
+
+                                                    <div class="ms-3 mt-3 ps-3 pt-3 bg-white border border-secondary-subtle rounded">
+                                                        <p><?= $comment["comment"] ?></p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </li>
